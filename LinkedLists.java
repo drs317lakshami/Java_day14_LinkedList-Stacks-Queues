9849b468d91519a1f4a07d0e1fa5f1926c5ec534
drs317lakshami <devrajkumar317@gmail.com>
2022-10-02 12:32:30 +0530

    Welcome to Data Structure Problems solving using Java Generics

---
+++ LinkedLists.java
@@ -0,0 +1,7 @@
+package com.java.day14.llist;
+
+public class LinkedLists<T> {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Data Structure Problems solving using Java Generics");
+			
+}}
