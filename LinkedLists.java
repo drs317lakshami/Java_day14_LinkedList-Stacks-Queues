bdfc34907f1c756768be9ba305e6c292fb955d77
drs317lakshami <devrajkumar317@gmail.com>
2022-10-03 23:08:16 +0530

    Ability to delete the first element from  56 30 and 70

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,106 +1,105 @@
 package com.java.day14.llist;
 import java.util.ArrayList;
 
-public class LinkedLists<T> {
+public class LinkedLists<E> {
 
-	Node<T> head;
+  Node<E> head;
 
-	public void addLast(T item) {
-		Node<T> newNode = new Node<>(item);
-		if (head == null)
-			head = newNode;
-		else if (head.next == null)
-			head.next = newNode;
-		else {
-			Node<T> temp = head;
-			while (temp.next != null)
-				temp = temp.next;
-			temp.next = newNode;
-		}
-	}
+  public void addFirst(E item) {
+    Node<E> newNode = new Node<>(item);
 
-	private boolean isEmpty() {
-		if (head == null)
-			return true;
-		return false;
-	}
+    if (head == null) head = newNode;//list is empty condition
+    else {
+      newNode.next = head;
+      head = newNode;
+    }
+  }
 
-	public void addFirst(T item) {
-		Node<T> newNode = new Node<>(item);
+  public void addLast(E item) {
+    Node<E> newNode = new Node<>(item);
 
-		if (head == null)
-			head = newNode;
-		else {
-			newNode.next = head;
-			head = newNode;
-		}
-	}
+    if (head == null) head = newNode;
+    else if (head.next == null) head.next = newNode;  
+    else {
+      Node<E> temp = head;
+      while (temp.next != null) temp = temp.next; 
+      temp.next = newNode;
+    }
+  }
+  public void insertAt(int index, E item) {
+    int count = 0;
+	if (index < 1 || index > count + 1) {
+      System.out.println( ("invalid index" + ( count + 1)));
+      return;
+    } else if (index == 1) {
+      if (head == null) System.out.println("list empty! adding " + item + " at index 1.");
+      addFirst(item);
+    } else if (index == count + 1) addLast(item);
+    else {
+      Node<E> newNode = new Node<>(item);
+      Node<E> temp = head;
 
-	public void display() {
-		if (isEmpty()) {
-			System.out.println("list is empty! ");
-			return;
-		}
+      for (int i = 1; i < index - 1; i++) temp = temp.next;
+      newNode.next = temp.next;
+      temp.next = newNode;
+      count++;
+    }
+  }
 
-		Node<T> temp = head;
-		while (temp.next != null) {
-			System.out.print(temp.data + " -> ");
-			temp = temp.next;
-		}
-		System.out.println(temp.data);
-	}
+  public void display() {
+    if (isEmpty()) {
+      System.out.println("list empty! nothing to display");
+      return;
+    }
 
-	public void displayFromEnd() {
-		if (isEmpty()) {
-			System.out.println("list is empty! ");
-			return;
-		}
+    Node<E> temp = head;
+    while (temp.next != null) {
+      System.out.print(temp.data + " -> ");
+      temp = temp.next;
+    }
+    System.out.println(temp.data);
+  }
 
-		ArrayList<T> nodes = new ArrayList<>();
-		Node<T> temp = head;
+  public void displayFromEnd() {
+    if (isEmpty()) {
+      System.out.println("list empty! nothing to display");
+      return;
+    }
 
-		while (temp.next != null) {
-			nodes.add(temp.data);
-			temp = temp.next;
-		}
-		nodes.add(temp.data);
+    ArrayList<E> nodes = new ArrayList<>(); 
+    Node<E> temp = head;
+    
+    while (temp.next != null) {
+        nodes.add(temp.data); 
+        temp = temp.next;
+      }
+    nodes.add(temp.data); 
+    for (int i = nodes.size() - 1; i > 0; i--) {
+      System.out.print(nodes.get(i) + " -> ");
+    }
+    System.out.println(nodes.get(0));
+  }
 
-		for (int i = nodes.size() - 1; i > 0; i--) {
-			System.out.print(nodes.get(i) + " -> ");
-		}
-		System.out.println(nodes.get(0));
-	}
+  public void deleteFirst() {
+    if (head == null) {
+      System.out.println("list empty! nothing to delete");
+      return;
+    } else head = head.next;
+  }
 
-	public void insertAt(int index, T item) {
-		int count = 0;
-		if (index < 1 || index > count + 1) {
-			System.out.println("invalid index" + (count + 1));
-			return;
-		} else if (index == 1) {
-			if (head == null)
-				System.out.println("list empty! adding " + item + " at index 1.");
-			addFirst(item);
-		} else if (index == count + 1)
-			addLast(item);
-		else {
-			Node<T> newNode = new Node<>(item);
-			Node<T> temp = head;
+  private boolean isEmpty() {
+    if (head == null) return true;
+    else return false;
+  }
 
-			for (int i = 1; i < index - 1; i++)
-				temp = temp.next;
-			newNode.next = temp.next;
-			temp.next = newNode;
-			count++;
-		}
-	}
 }
 
-class Node<T> {
+class Node<E> {
 
-	T data;
-	Node<T> next;
+  E data;
+  Node<E> next;
 
-	Node(T data) {
-		this.data = data;
-	}
+  Node(E data) {
+    this.data = data;
+  }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.java.day14.llist;
 
+
 public class Main {
 
 	  public static void main(String[] args) {
@@ -7,10 +8,14 @@ public class Main {
 	    LinkedLists<Integer> linkedL = new LinkedLists<>();
 
 	    linkedL.addLast(56);
+	    linkedL.addLast(30);
 	    linkedL.addLast(70);
 	    linkedL.display();
 
 	    linkedL.insertAt(2, 30);
+	    linkedL.display();
+
+	    linkedL.deleteFirst();
 	    linkedL.display();
 
 	  }
