fa35d0ec141c630f5107402b0423bb0c1e49c5ac
drs317lakshami <devrajkumar317@gmail.com>
2022-10-04 22:48:38 +0530

    Ability to create a Stack of 56->30->70

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.java.day14.stacks;
+
+
+public class Main {
+
+	  public static void main(String[] args) {
+	    Stacks<Integer> stk = new Stacks<>();
+
+	    stk.push(70);
+	    stk.push(30);
+	    stk.push(56);
+	    
+	    stk.display();
+
+	  }
+	}
