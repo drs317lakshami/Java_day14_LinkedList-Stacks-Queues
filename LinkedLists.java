53be7acc14979372ff54753295967ecf0e00c59d
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-04 22:25:38 +0530

    Update LinkedLists.java

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,55 +1,63 @@
-package com.java.day14.llist;
+package com.java.day14.stacks;
 
 import java.util.ArrayList;
 
 public class LinkedLists<E> {
 
   Node<E> head;
-  private int count;
 
-  public void addFirst(E item) {
-    Node<E> newNode = new Node<>(item);
+  public void addLast(E data) {
+    Node<E> newNode = new Node<>(data);
 
-    if (head == null) head = newNode;//list is empty condition
+    if (head == null) head = newNode; //list is empty
+
+    else if (head.next == null) head.next = newNode; //node being added is the second node.
+
     else {
-      newNode.next = head;
-      head = newNode;
+      Node<E> lastNode = traverseTillLast(); //calling traverseTillLast to get the last node
+      lastNode.next = newNode;
     }
-    count++;
-    sort();
   }
 
-  public void addLast(E item) {
-    Node<E> newNode = new Node<>(item);
-
-    if (head == null) head = newNode;//node being added is the first node
-    else if (head.next == null) head.next = newNode;  //node being added is the second node
-    else {
-      Node<E> temp = head;
-      while (temp.next != null) temp = temp.next; //traverse to the last node
-      temp.next = newNode;
-    }
-    count++;
-    sort();
-  }
-
-  public void insertAt(int index, E item) {
-    if (index < 1 || index > count + 1) {
-      System.out.println("invalid index. enter between 1 and " + (count + 1));
+  public void peek() {
+    if (isEmpty()) {
+      System.out.println("list empty!");
       return;
-    } else if (index == 1) {
-      if (head == null) System.out.println("list empty! adding " + item + " at index 1.");
-      addFirst(item);
-    } else if (index == count + 1) addLast(item);
-    else {
-      Node<E> newNode = new Node<>(item);
-      Node<E> temp = head;
-
-      for (int i = 1; i < index - 1; i++) temp = temp.next;
-      newNode.next = temp.next;
-      temp.next = newNode;
-      count++;
     }
+    Node<E> lastNode = traverseTillLast();
+    System.out.println("element at top: " + lastNode.data);
+
+  }
+
+  public void removeFirst() {
+    if (isEmpty()) {
+        System.out.println("list empty! nothing to remove");
+        return;
+    }
+    System.out.println("removed " + head.data);
+    head = head.next;
+  }
+
+  public void removeLast() {
+    if (isEmpty()) {
+      System.out.println("list empty! nothing to remove");
+      return;
+    }
+    if (head.next == null) {  //list has just one node
+      System.out.println("removed " + head.data);
+      head = null;
+    } else {
+      Node<E> temp = head;
+        while (temp.next.next != null) temp = temp.next;  //traversing till the 2nd last node.
+        System.out.println("removed " + temp.next.data);
+        temp.next = null;
+    }
+  }
+
+  private Node<E> traverseTillLast() {
+    Node<E> temp = head;
+    while (temp.next != null) temp = temp.next;  //traversing till the last node.
+    return temp;
   }
 
   public void display() {
@@ -74,7 +82,6 @@ public class LinkedLists<E> {
 
     ArrayList<E> nodes = new ArrayList<>(); //creating an empty ArrayList, to the node elements.
     Node<E> temp = head;
-    
     while (temp.next != null) {
         nodes.add(temp.data); //adding nodes to arraylist till current node's next points to null
         temp = temp.next;
@@ -87,138 +94,11 @@ public class LinkedLists<E> {
     System.out.println(nodes.get(0));
   }
 
-  public void pop() {
-    if (head == null) {
-      System.out.println("list empty! nothing to delete");
-      return;
-    } else {
-      Node<E> temp = head;
-
-      head = head.next;
-      temp.next = null;
-      count--;
-    }
-  }
-
-  public void popLast() {
-    if (head == null) {
-      System.out.println("list empty! nothing to delete");
-      return;
-    } else if (head.next == null) {
-      System.out.println("deleted " + head.data);
-      head = head.next;
-      count--;
-    }
-    else {
-      Node<E> temp = head;
-
-      while (temp.next.next != null) temp = temp.next;
-      System.out.println("deleted " + temp.next.data);
-      temp.next = null;
-      count--;
-    }
-  }
-
-  public void deleteNode(E nodeTodelete) {
-    if (isEmpty()) {
-      System.out.println("list empty! cant delete " + nodeTodelete);
-    } else if (head.data == nodeTodelete) {
-      System.out.println("deleted " + nodeTodelete + " from index " + 1);
-      pop();
-    } else {
-      Node<E> temp = head;
-      Node<E> temp2 = head.next;
-      for (int i = 2; i <= count; i++) {    //looping until we find the node to delete.
-        if (temp2.data == nodeTodelete) {
-          System.out.println("deleted " + nodeTodelete + " from index " + i);
-          temp.next = temp2.next;
-          count--;
-          return;
-        }
-        temp = temp.next;
-        temp2 = temp2.next;
-      }
-      System.out.println("we couldnt find " + nodeTodelete + " in the list.");    //when looped till the end and couldn't find the node
-    }
-  }
-
-  public void findNode(E nodeToFind) {
-    if (isEmpty()) {
-      System.out.println("list empty! cant add after " + nodeToFind);
-    } else {
-      Node<E> temp = head;
-      for (int i = 1; i <= count; i++) {    //looping until we find the given node.
-        if (temp.data == nodeToFind) {
-          System.out.println(nodeToFind + " found at index " + i);
-          return;
-        }
-        temp = temp.next;
-      }
-      System.out.println("we couldnt find " + nodeToFind + " in the list.");    //when looped till the end and couldn't find the node
-    }
-  }
-
-  public void addNodeAfter(E prevNode, E nodeToAdd) {
-    if (isEmpty()) {
-      System.out.println("list empty! cant add after " + prevNode);
-    } else {
-      Node<E> temp = head;
-
-      for (int i = 1; i <= count; i++) {    //looping until we find the node to add after.
-        if (temp.data == prevNode) {
-          Node<E> newNode = new Node<>(nodeToAdd);
-          newNode.next = temp.next;
-          temp.next = newNode;
-          System.out.println(prevNode + " found at index " + i + ". added " + nodeToAdd + " at index " + (i + 1));
-          count++;
-          return;
-        }
-        temp = temp.next;
-      }
-      System.out.println("we couldnt find " + prevNode + " in the list. perhaps try a different node"); //when looped till the end and didnt find the node
-    }
-  }
-
-  public short size() {
-    if (isEmpty()) return 0;
-    else if (head.next == null) return 1;
-    else {
-      short size = 1;
-      Node<E> temp = head;
-      for (short i = 0; i <= count; i++) {
-        if (temp.next == null) return size;
-        temp = temp.next;
-        size++;
-      }
-      return size;
-    }
-  }
-
-  @SuppressWarnings({"unchecked", "rawtypes", "hiding"})
-  public <E extends Comparable> void sort() {
-    boolean swapped = false;
-    do {
-      Node<E> fNode = (Node<E>) head;
-      Node<E> sNode = (Node<E>) head.next;
-      swapped = false;
-      for (short i = 2; i <= count; i++) {
-        if (fNode.data.compareTo(sNode.data) > 0) {
-          E temp = fNode.data;
-          fNode.data = sNode.data;
-          sNode.data = temp;
-          swapped = true;
-        }
-        fNode = fNode.next;
-        sNode = sNode.next;
-      }
-    } while (swapped);
-  }
-
   private boolean isEmpty() {
     if (head == null) return true;
     else return false;
   }
-
+  
 }
 
 class Node<E> {
@@ -226,7 +106,7 @@ class Node<E> {
   E data;
   Node<E> next;
 
-  Node(E data) {
+  public Node(E data) {
     this.data = data;
   }
 }
