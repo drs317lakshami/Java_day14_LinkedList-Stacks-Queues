e57e82e035158a48393f4b275a14e470cca612a7
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-04 22:35:13 +0530

    Welcome to Stacks and Queues programs

---
+++ Queues.java
@@ -0,0 +1,19 @@
+package com.java.day14.stacks;
+
+public class Queues<E> {
+
+	  LinkedLists<E> linkedL = new LinkedLists<>();
+
+	  public void enqueue(E item) {
+	    linkedL.addLast(item);
+	  }
+
+	  public void display() {
+	    linkedL.display();
+	  }
+	  
+	  public void dequeue() {
+	    linkedL.removeFirst();
+	  }
+
+	}
