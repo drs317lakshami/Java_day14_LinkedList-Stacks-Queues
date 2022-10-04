69c39b5c4965b0bb71321df03301b7b8c70fcf6a
drs317lakshami <devrajkumar317@gmail.com>
2022-10-04 23:08:35 +0530

    Ability to create a Queue of 56 30 70

---
+++ Queue.java
@@ -0,0 +1,15 @@
+package com.java.day14.stacks;
+
+public class Queue<E> {
+
+  LinkedLists<E> linkedL = new LinkedLists<>();
+
+  public void enqueue(E item) {
+    linkedL.addLast(item);
+  }
+
+  public void display() {
+    linkedL.display();
+  }
+
+}
