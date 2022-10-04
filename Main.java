68ffe9b73ddfadfcdae52b47ac9b10b4bd1685a8
drs317lakshami <devrajkumar317@gmail.com>
2022-10-04 22:29:35 +0530

    Welcome to Stacks and Queues program

--- Main.java
+++ Main.java
@@ -1,17 +1,47 @@
-package com.java.day14.llist;
+package com.java.day14.stacks;
+
 
 public class Main {
 
 	  public static void main(String[] args) {
-	    LinkedLists<Integer> linkedL = new LinkedLists<>();
+	    System.out.println("-STACK OPERATIONS-");
+	    Stacks<Integer> st = new Stacks<>();
+	    st.push(70);
+	    st.push(30);
+	    st.push(56);
 
-	    
-	    linkedL.addLast(56);
-	    linkedL.addLast(30);
-	    linkedL.addLast(40);
-	    linkedL.addLast(70);
-	    linkedL.display();
+	    st.peek();
+
+	    st.display();
+	    st.pop();
+	    st.peek();
+
+	    st.display();
+	    st.pop();
+
+	    st.display();
+	    st.pop();
+
+	    st.display();
+	    st.pop();
+
+	    System.out.println("\n-QUEUE OPERATIONS-");
+	    Queues<Integer> que = new Queues<>();
+	    que.enqueue(56);
+	    que.enqueue(30);
+	    que.enqueue(70);
+
+	    que.display();
+	    que.dequeue();
+
+	    que.display();
+	    que.dequeue();
+
+	    que.display();
+	    que.dequeue();
+
+	    que.display();
+	    que.dequeue();
 
 	  }
-
 	}
---
+++ Stacks.java
@@ -0,0 +1,22 @@
+package com.java.day14.stacks;
+
+public class Stacks<E> {
+
+	  LinkedLists<E> linkedL = new LinkedLists<>();
+
+	  public void push(E data) {
+	    linkedL.addLast(data);
+	  }
+
+	  public void display() {
+	    linkedL.displayFromEnd();
+	  }
+	  
+	  public void peek() {
+	    linkedL.peek();
+	  }
+
+	  public void pop() {
+	    linkedL.removeLast();
+	  }
+	}
